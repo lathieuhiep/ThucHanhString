db754dadbb7af27043f0fa7af92db1498ba0f68e
lathieuhiep <khacdiepkma90@gmail.com>
2024-05-29 16:55:38 +0700

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Text text1 = new Text("abc123nd4gg56");
+        System.out.println("Chuỗi ban đầu: " + text1.getText());
+
+        text1.upperCaseChar();
+        text1.reverse();
+
+        System.out.println("\n");
+        Text text2 = new Text("nguyen van thanh");
+        System.out.println("Chuỗi ban đầu: " + text2.getText());
+        text2.upperCaseFirstChars();
+
+        System.out.println("\n");
+        Text text3 = new Text("Hello e hH");
+        System.out.println("Chuỗi ban đầu: " + text3.getText());
+        text3.reverseCase();
+
+        System.out.println("\n");
+        Text text4 = new Text("34598A3");
+        System.out.println("Chuỗi ban đầu: " + text4.getText());
+        text4.sortCharDesc();
+
+        System.out.println("\n");
+        Text text5 = new Text("Nguyễn Văn Thành_A");
+        System.out.println("Chuỗi ban đầu: " + text5.getText());
+        text5.printWord();
+
+        System.out.println("\n");
+        Text text6 = new Text("  Nguyễn  V ăn   Thành_A_  ");
+        System.out.println("Chuỗi ban đầu: " + text6.getText());
+        text6.trimExtra();
+
+        System.out.println("\n");
+        Text text7 = new Text("Lap trinh huong doi tuong");
+        System.out.println("Chuỗi ban đầu: " + text7.getText());
+        text7.findBigWord();
     }
 }
---
+++ src/Text.java
@@ -0,0 +1,175 @@
+public class Text {
+    private String text;
+
+    public Text(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    // viet hoa chu cai trong chuoi
+    public void upperCaseChar() {
+        String upperCaseChar = text.toUpperCase();
+
+        System.out.println("Chuỗi viết hoa: " + upperCaseChar);
+    }
+
+    // chuoi dao nguoc
+    public void reverse() {
+        System.out.print("Chuỗi đảo ngược: ");
+
+        for (int i = text.length() - 1; i >= 0 ; i--) {
+            char characters = text.charAt(i);
+
+            System.out.print(characters);
+        }
+    }
+
+    // viet hoa chu cai dau cua chuoi
+    public void upperCaseFirstChars() {
+        String kq = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            String characters = text.charAt(i) + "";
+
+            if (i == 0 || text.charAt(i - 1) == ' ') {
+                characters = characters.toUpperCase();
+            }
+
+            kq += characters;
+        }
+
+        System.out.println("Chuỗi in hoa đầu mỗi kí tự: " + kq);
+    }
+
+    // Đảo ngược ký tự hoa thường trong chuỗi
+    public void reverseCase() {
+        String kq = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            String characters = text.charAt(i) + "";
+            String newCharacters = characters.toUpperCase();
+
+            if ( newCharacters.equals(characters) ) {
+                newCharacters = characters.toLowerCase();
+            }
+
+            kq += newCharacters;
+        }
+
+        System.out.println("Chuỗi biến đổi hoa thường: " + kq);
+    }
+
+    // Sắp xếp và in ra các ký tự trong chuỗi theo mã ASCII giảm dần
+    public void sortCharDesc() {
+        String kq = "";
+
+        while (!text.isEmpty()) {
+            String minChars = findMinChars();
+            kq += minChars;
+        }
+
+        System.out.println("Chuỗi tăng dần: " + kq);
+    }
+
+    private String findMinChars() {
+        char min = text.charAt(0);
+        String minChars = min + "";
+
+        for (int i = 1; i < text.length(); i++) {
+            char nam = text.charAt(i);
+
+            if ( nam < min ) {
+                minChars = nam + "";
+                min = nam;
+            } else if ( nam == min ) {
+                minChars += nam;
+            }
+        }
+
+        text = text.replace(min + "", "");
+        return minChars;
+    }
+
+    // In ra từng từ trong chuỗi biết rằng mỗi từ được hiểu là cách nhau bằng dấu cách
+    public void printWord() {
+        String gioTu = "";
+
+        System.out.print("Chuỗi tách từ bằng dấu cách: ");
+
+        for (int i = 0; i < text.length(); i++) {
+            char nam = text.charAt(i);
+
+            if ( nam != ' ' ) {
+                gioTu += nam;
+
+                if ( i == text.length() - 1 ) {
+                    System.out.print(gioTu);
+                }
+
+            } else if (!gioTu.isEmpty()) {
+                System.out.print(gioTu + ", ");
+                gioTu = "";
+            }
+        }
+    }
+
+    // Xóa các ký tự dấu cách thừa trong chuỗi
+    public void trimExtra() {
+        String chuoiMoi = "";
+        text = text.trim();
+
+        System.out.print("Chuỗi xóa các ký tự dấu cách thừa: ");
+
+        for (int i = 0; i < text.length(); i++) {
+            char nam = text.charAt(i);
+
+            if ( i > 0 && nam == ' ' ) {
+                char namTruoc = text.charAt(i - 1);
+
+                if ( namTruoc != ' ' ) {
+                    chuoiMoi += nam;
+                }
+            } else {
+                chuoiMoi += nam;
+            }
+        }
+
+        System.out.println(chuoiMoi);
+    }
+
+    // tìm và in ra từ có nhiều ký tự nhất trong chuỗi
+    public void findBigWord() {
+        String gioBigWord = "";
+        String gioTu = "";
+        int gioMax = 0;
+
+        System.out.print("Từ có nhiều ký tự nhất trong chuỗi: ");
+
+        text += " ";
+        for (int i = 0; i < text.length(); i++) {
+            char nam = text.charAt(i);
+
+            if ( nam != ' ' ) {
+                gioTu += nam;
+            } else if (!gioTu.isEmpty()) {
+                if ( gioTu.length() > gioMax ) {
+                    gioBigWord = gioTu;
+                    gioMax = gioTu.length();
+                } else if (gioTu.length() == gioMax) {
+                    gioBigWord += " ," + gioTu;
+                }
+
+                gioTu = "";
+            }
+        }
+
+        System.out.println(gioBigWord);
+    }
+}
